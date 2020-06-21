b6e2c3edf44024293a362e7d2e9b0aaa28f8d984
kmalith <kmalith@virtusa.com>
2020-06-21 18:28:04 +0530

    add navigation bar

--- OnlineShopping/src/app/app.module.ts
+++ OnlineShopping/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsHomeComponent } from './products-home/products-home.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsHomeComponent
+    ProductsHomeComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule,
